af12082853b4ea6d32370a861d802b98bf932e08
Berna <bernasurfpix@gmail.com>
2021-10-21 13:53:24 +1000

    add base structure with mock data

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -3,29 +3,31 @@ import * as S from './styled';
 
 const Profile = () => {
   return (
-    <S.Wrapper>
-      <div>
-        <img src="https://avatars.githubusercontent.com/u/67717387?v=4" alt="User Avatar" />
+    <S.Wrapper>    
+      <img src="https://avatars.githubusercontent.com/u/67717387?v=4" alt="User Avatar" />
         <div>  
           <h1>Nome Usuario</h1>
-          <h3>Username: </h3>
-          <span>username</span>
+          <S.WrapperUserName>
+            <h3>Username: </h3>
+            <span>username</span>
+          </S.WrapperUserName>
         </div>
-        <div>
-          <h4>Followers:</h4>
-          <span>#followers</span>
-        </div>  
-        <div>
-          <h4>Starred:</h4>
-          <span>#starred</span>
-        </div>  
-        <div>
-          <h4>Following:</h4>
-          <span>#following</span>
-        </div>  
+        <S.WrapperUserNumbers>
+          <div>
+            <h4>Followers</h4>  
+            <span>#followers</span>
+          </div>
+          <div>
+            <h4>Starred</h4>
+            <span>#starred</span>
+          </div>
+          <div>
+            <h4>Following</h4>
+            <span>#following</span>
+          </div>  
+        </S.WrapperUserNumbers>
         <div>Repositories</div>
         <div>Starreds</div>
-      </div>
     </S.Wrapper>
   )
 }
--- src/components/profile/styled.js
+++ src/components/profile/styled.js
@@ -1,5 +1,19 @@
 import styled from 'styled-components';
 
-export const Wrapper =styled.div`
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+`;
 
+export const WrapperUserName = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const WrapperUserNumbers = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  div {
+    margin-left: 8px;
+  }
 `;
