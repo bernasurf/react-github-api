f7db924344698eec0dfbc4b8b51dadfa182d7ebd
Berna <bernasurfpix@gmail.com>
2021-10-22 10:19:18 +1000

    resetCSS using createGlobalStyle from styled-components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { ResetCSS } from './global/resetCSS';
 import Layout from './components/layout';
 import Profile from './components/profile';
 
+
 function App() {
   return (
+    <main>
+      <ResetCSS />
       <Layout>
         <Profile></Profile>
       </Layout>
+    </main>
   );
 }
 
